800807c588545cd8f92c1e3274470abdd27af18f
root <root@mec-w1.cluster.local>
2020-10-19 14:37:28 +0900

    etri-version-1

--- vul_detect.sh
+++ vul_detect.sh
@@ -3,15 +3,13 @@
 a="\"http://localhost/api/v4/projects/1/registry/repositories/1/tags/${1:27}\""
 b="curl --request DELETE --header \"PRIVATE-TOKEN: Kca5ANW3gda32Jn3M6C9\" $a"
 
+cd ./clair
+clairctl-linux-amd64 --config=clairctl.yml analyze $1>result.txt
+clairctl-linux-amd64 --config=clairctl.yml report $1
+python filter.py
+python parse.py>score.txt
+file="score.txt"
 
-cd /root/clair-and-docker-notary-example/
-docker-compose run --rm clair-scanner $1>/root/gitlab/docker_imagetest.txt
-cd /root/gitlab/
-sed '/Unapproved/!d' docker_imagetest.txt>result.txt
-python confirm.py>1.txt
-python tx.py>$1_score.txt
-sed -n 2p $1_score.txt>score.txt
-file="$1_score.txt"
 while IFS= read -r line
 do
 	if [ "$line" == "Delete your docker image" ]; then
